230d546fff79cb948dc365d89ed5473fa2ca9aac
TheAtom111 <79643477+TheAtom111@users.noreply.github.com>
2023-07-18 11:53:45 +0800

    Function tcikValue, Function power

---
+++ Card.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+# Function: tickValue
+func tick():
+	var tickValue = 5
+	
+# Function: power
+func power():
+	var power = 1
+	
+	
+	
